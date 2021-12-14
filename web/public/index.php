355a595a9e888163997ac8f00fdecf8261907c18
Ahmet Kamis <ahmetkamis@Ahmets-MacBook-Pro-2.local>
2021-12-14 10:07:35 -0600

    docker files

---
+++ web/public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// require_once "../vendor/autoload.php";
+
+// phpinfo();
+
+echo "Hello World <br />";
+
+// Test Mysql Connection
+
+// try {
+//     $dbh = new PDO('mysql:host=mysqldb;dbname=test', "root", "root");
+//     echo "Mysql Connected";
+
+// } catch (PDOException $e) {
+//     print "Error!: " . $e->getMessage() . "<br/>";
+//     die();
+// }
